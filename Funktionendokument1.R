7a2a3a555dadfd54a8f36dcc617cca0683dc37a0
JuliaKeiter <Julia.Keiter@gmx.de>
2021-02-09 16:51:33 +0100

    Update Funktionendokument1.R

--- Funktionendokument1.R
+++ Funktionendokument1.R
@@ -15,3 +15,18 @@ Funktion1 <- function(x){
 }
 
 Funktion1(x)
+
+x
+y <- c(1:5,10)
+y
+
+#c)
+Funktion3 <- function(x){
+  print("Kovarianz Bravais-Pearson" <- cov(x, y, method = "pearson"))
+  print("Kovarianz Kendall" <- cov(x,y, method = "kendall"))
+  print("Kovarianz Spearman" <- cov(x,y, method = "spearman"))
+  print("Korrelation" <- cor(x,y))
+}
+
+
+Funktion3(x)
