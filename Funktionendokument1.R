b64053b6524d18a9dd91b74794ae7b1a443bdfba
aliciahemmersbach <75793472+aliciahemmersbach@users.noreply.github.com>
2021-02-09 17:37:56 +0100

    Update Funktionendokument1.R

--- Funktionendokument1.R
+++ Funktionendokument1.R
@@ -12,6 +12,16 @@ Funktion1 <- function(x){
 
 Funktion1(x)
 
+#b)
+
+funktion2 <- function(x,...){
+  haeuf <- table(x)      #haufigkeitstabelle
+  modal <- max(x)        #modalwert
+  print(list("Tabelle"= haeuf, "Modalwert"=modal))
+}
+
+funktion2(x)
+
 #c)
 Funktion3 <- function(x,y){
   print("Korrelation Bravais-Pearson" <- cor(x, y, method = "pearson"))
@@ -23,6 +33,24 @@ Funktion3(x,y)
 
 #Kovarianzen miteinander vergleichen
 #Die Kovarianz von 
+
+#d)
+
+funktion4 <- function(x,y,...){
+  if(!is.numeric(x) && !is.numeric(y)) stop("Mindestens ein Objekt muss numerisch sein")
+  if(!(length(x)==length(y))) stop("Objekte muessen die selbe Laenge haben")
+  gr <- round(min(x)+(range(x)[2]-range(x)[1])/2,digits=0)
+  a <- length(which(x < gr & y == T))
+  b <- length(which(x < gr & y == F))
+  c <- length(which(x >= gr & y == T))
+  d <- length(which(x >= gr & y == F))
+  rG <- (a*d-b*c)/sqrt((a+b)*(c+d)*(a+c)*(b+d))  #gruppierter Bravais Pearson
+  print(rG)
+}
+
+funktion4(x,y)
+
+#gibt gruppierten Bravais Pearson Korrelationskoeffizienten aus
 
 #e)
 
